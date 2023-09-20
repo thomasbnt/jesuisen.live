f1e5e45120fee56e7b2e44508d651e79809d03c9
Thomas Bnt <thomasbnt@protonmail.com>
2023-09-20 23:55:28 +0200

    About route, ejs

--- src/app.js
+++ src/app.js
@@ -1,27 +1,36 @@
 const path = require("node:path")
 const fastify = require('fastify')({logger: false})
+const fastifyView = require('@fastify/view')
 const fastifyStatic = require('@fastify/static')
+const ejs = require('ejs')
 
-
-fastify.register(require('@fastify/view'), {
-  engine: { ejs: require('ejs') },
+fastify.register(fastifyView, {
+  engine: { ejs },
 })
 
 fastify.register(fastifyStatic, {
   root: path.join(__dirname, 'public')
 })
 
-// Index
+// Index page
 fastify.get('/', (request, reply) => {
   reply.sendFile('index.html')
+})
+
+// About page
+fastify.get('/about', (request, reply) => {
+  reply.sendFile('about.html')
 })
 
 // /u/:username route
 fastify.register(require('./routes/username'))
 
+
 const start = async () => {
   try {
-    await fastify.listen({port: 3000})
+  await fastify.listen({port: 3000}, (err, address) => {
+    console.log(`Server listening on ${address}`)
+  })
   } catch (err) {
     fastify.log.error(err)
     process.exit(1)
