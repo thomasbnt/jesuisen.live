ee805f1f353bd2962746dea76582bc2cdb06fe4d
Thomas Bnt <thomasbnt@protonmail.com>
2023-09-20 23:54:40 +0200

    username became username.ejs

--- src/routes/username.js
+++ src/routes/username.js
@@ -1,15 +1,44 @@
 module.exports = function (fastify, opts, done) {
-  fastify.get('/u/:username', async function (request, reply) {
-    const username = request.params.username
-    if (!username) {
-      reply.code(400)
-      return reply.sendFile('error/400.html')
+  fastify.get('/u/:username', async function (req, res) {
+    const data = await CheckIfUserExists(req, res, req.params.username)
+    console.log(data)
+    if (data.status === 400) {
+      res.code(400)
+      return res.view('src/templates/error/404.ejs', {message: data.message})
     }
-    const data = await TwitchAPI(username)
-    reply.send(data)
+    if (!data) {
+      res.code(404)
+      return res.view('src/templates/error/404.ejs')
+    }
+    return res.view('src/templates/username.ejs', {
+      data: data.data[0],
+      username: data.data[0].login,
+      title: `${data.data[0].display_name} on Twitch`,
+      url: `https://twitch.tv/${data.data[0].login}`,
+      image: data.data[0].profile_image_url,
+      description: data.data[0].description || `Twitch profile of ${data.data[0].display_name}`,
+    })
+  })
+  done()
+
+  // /u/:username/json
+  fastify.get('/u/:username/json', async function (req, res) {
+    console.log('JSON username')
+    const data = await CheckIfUserExists(req, res, req.params.username)
+    return res.send({data: data.data[0]})
   })
   done()
 }
+
+
+async function CheckIfUserExists(req, res, username) {
+  if (!username) {
+    res.code(400)
+    return res.view('src/templates/error/400.ejs')
+  }
+  return await TwitchAPI(username)
+}
+
 
 async function TwitchAPI(username) {
   require('dotenv').config()
@@ -24,7 +53,6 @@ async function TwitchAPI(username) {
   params.append('client_secret', process.env.TWITCH_CLIENT_SECRET)
   params.append('grant_type', 'client_credentials')
   const responseToken = await fetch(urlToken, {method: 'POST', body: params})
-  console.log(responseToken)
   const dataToken = await responseToken.json()
   const token = dataToken.access_token
 
